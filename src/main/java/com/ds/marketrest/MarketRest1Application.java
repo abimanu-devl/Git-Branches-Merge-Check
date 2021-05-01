4a03b85262533823bfc7262287952d0361f89ffb
abimanu-devl <abishekmanusha@gmail.com>
2021-05-01 12:31:28 +0530

    test git

---
+++ src/main/java/com/ds/marketrest/MarketRest1Application.java
@@ -0,0 +1,13 @@
+package com.ds.marketrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketRest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/marketrest/MarketRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.marketrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
